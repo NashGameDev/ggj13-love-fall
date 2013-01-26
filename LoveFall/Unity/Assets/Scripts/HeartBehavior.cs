ca897758405ccb3d9427d7af9a240193827ba405
Joshua <jsnichols87@gmail.com>
2013-01-26 14:44:25 -0600

    Heart Scripts

---
+++ LoveFall/Unity/Assets/Scripts/HeartBehavior.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public class HeartBehavior: MonoBehaviour {
+	
+	public bool allowDebug = true;
+
+	// Update is called once per frame
+	void Update () {
+		
+		//Rotation
+		transform.Rotate(Vector3.up * Time.deltaTime * 60);
+
+		
+	}
+	
+	void OnTriggerEnter(Collider collider){
+		switch(collider.gameObject.tag){
+			case "Player":
+			ScoreController.score++;
+			Destroy(this.gameObject);
+			
+			break;
+		}
+		
+	}
+	
+
+	
+}
